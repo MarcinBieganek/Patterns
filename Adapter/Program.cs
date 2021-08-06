aadcf0422868c8c71ea0c8ca9185ded56d9dc5eb
MarcinBieganek <45421856+MarcinBieganek@users.noreply.github.com>
2021-08-06 13:48:07 +0200

    Add Adapter code

---
+++ Adapter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace Zadanie_4
+{
+    public class IntSorter : IComparer
+    {
+        Comparison<int> comparison;
+
+        public IntSorter(Comparison<int> comp)
+        {
+            this.comparison = comp;
+        }
+        int IComparer.Compare(object x, object y)
+        {
+            return this.comparison((int) x, (int) y);
+        }
+    }
+    class Program
+    {
+        /* this is the Comparison<int> to be converted */
+
+        static int IntComparer( int x, int y )
+        {
+            return x.CompareTo( y );
+        }
+
+        static void Main( string[] args )
+        {
+            ArrayList a = new ArrayList() { 1, 5, 3, 3, 2, 4, 3 };
+
+            a.Sort( new IntSorter(IntComparer) );
+
+            foreach(var item in a )
+            {
+                Console.Write(item + " ");
+            }
+
+        }
+    }
+}
