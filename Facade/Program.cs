64b56af2e13d7392127b2664f0cf7bdb47605fe2
MarcinBieganek <45421856+MarcinBieganek@users.noreply.github.com>
2021-08-06 13:45:33 +0200

    Add Facade code.

---
+++ Facade/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net.Mail;
+using System.Net.Mime;
+
+namespace Zadanie_1
+{
+    public class SmtpClientFacade
+    {
+        public void Send(string From, string To, string Subject, 
+                         string Body, Stream Attachment, string AttachmentMimeType)
+        {
+            SmtpClient client   = new SmtpClient();
+            MailMessage message = new MailMessage(From, To, Subject, Body);
+
+            ContentType ct      = new ContentType(AttachmentMimeType);
+            Attachment data     = new Attachment(Attachment, ct);
+            message.Attachments.Add(data);
+
+            client.Send(message);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmtpClientFacade smtpclientf = new SmtpClientFacade();
+
+            Console.ReadLine();
+        }
+    }
+}
